a4357eb8ef2055a8139d53c8cea1f9ed1ffcdb7e
nmkip <juan.ignacio848@gmail.com>
2019-01-11 23:22:58 -0300

    primefactors kata

--- lib/primefactors.rb
+++ lib/primefactors.rb
@@ -1,6 +1,20 @@
-require "primefactors/version"
+class Primefactors
 
-module Primefactors
-  class Error < StandardError; end
-  # Your code goes here...
+  def of(n)
+    if n == 1
+      return []
+    end
+
+    factors = []
+    divisor = 2
+    while n > 1
+      while n % divisor == 0
+        factors.append(divisor)
+        n = n / divisor
+      end
+      divisor += 1
+    end
+
+    factors
+  end
 end
--- test/primefactors_test.rb
+++ test/primefactors_test.rb
@@ -1,11 +1,41 @@
 require "test_helper"
 
 class PrimefactorsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Primefactors::VERSION
+
+  def test_nothing
+    assert true
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+    @primefactors = Primefactors.new
   end
+
+  def test_primefactors_of_1_is_empty_list
+    assert_equal([], @primefactors.of(1))
+  end
+
+  def test_primefactors_of_2_is_list_2
+    assert_equal([2], @primefactors.of(2))
+  end
+
+  def test_primefactors_of_3_is_list_3
+    assert_equal([3], @primefactors.of(3))
+  end
+
+  def test_primefactors_of_4_is_list_2_2
+    assert_equal([2, 2], @primefactors.of(4))
+  end
+
+  def test_primefactors_of_6_is_list_2_3
+    assert_equal([2, 3], @primefactors.of(6))
+  end
+
+  def test_primefactors_of_9_is_list_3_3
+    assert_equal([3, 3], @primefactors.of(9))
+  end
+
+  def test_acceptance
+    assert_equal([2, 2, 2, 2, 3, 3, 3, 11, 13, 17], @primefactors.of(2*2*2*2*3*3*3*11*13*17))
+  end
+
 end
